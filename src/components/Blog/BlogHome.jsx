501a41f7c1d9be0c609cc7b7504b7347ddacd6dc
programer1983 <programer972@gmail.com>
2022-04-20 10:27:07 +0300

    commit-4;

--- src/components/Blog/BlogHome.jsx
+++ src/components/Blog/BlogHome.jsx
@@ -10,7 +10,7 @@ const BlogHome = () => {
       paraTwo: "New Ways to Wear Denim Belt Bags make a Comeback",
       name: "By Jasmin Smith",
       subTitle: "How to Shop Mindfully",
-      qrCode: "/images/qrcode.png",
+      qrCode: "/Images/qrcode.png",
       cover: "/Images/blog5.jpg",
     },
 
@@ -18,7 +18,33 @@ const BlogHome = () => {
   
   return (
     <section className="home">
-      <div className="left-content"></div>
+      <div className="left-content">
+        {data.map((value) => {
+          return (
+            <div className="content">
+              <div className="logo">
+                <h1>M</h1>
+              </div>
+              <div className="home-img">
+                <img src={value.cover} alt="" />
+              </div>
+              <div className="text">
+                <h1>{value.titleOne}</h1>
+                <p>{value.paraOne}</p>
+                <span>{value.name}</span>
+              </div>
+              <div className="text text2">
+                <h1>{value.titletwo}</h1>
+                <p>{value.paraTwo}</p>
+                <span>{value.subTitle}</span>
+              </div>
+              <div className="qrcode">
+                <img src={value.qrCode} alt="" />
+              </div>
+            </div>
+          )
+        })}
+      </div>
       <div className="right-content"></div>
     </section>
   )
